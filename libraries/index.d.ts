25ea1714037d108f3f6b30c7de8afb5b805eed91
hyochan <dooboolab@gmail.com>
2019-07-30 23:29:10 +0900

    Update readme and declare ts module

---
+++ libraries/index.d.ts
@@ -0,0 +1,11 @@
+import SwitchToggle from './switch-toggle';
+import DropdownItem from './dropdown-item';
+import Calendar from './calendar';
+
+declare module 'dooboo-native-widgets' {
+  export {
+    SwitchToggle,
+    DropdownItem,
+    Calendar,
+  };
+}
