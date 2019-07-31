ba138cef7c720cadef9551d170dc937234ec1613
hyochan <dooboolab@gmail.com>
2019-07-31 22:37:57 +0900

    Fix index.d.ts for [Calendar]

--- src/components/shared/calendar/index.d.ts
+++ src/components/shared/calendar/index.d.ts
@@ -1,31 +1,11 @@
 import * as React from 'react';
-import { GestureResponderEvent } from 'react-native';
 
 interface Props {
-    switchOn: boolean;
-    onPress: (event: GestureResponderEvent) => {};
-    containerStyle?: object;
-    circleStyle?: object;
-    backgroundColorOn?: string;
-    backgroundColorOff?: string;
-    circleColorOff?: string;
-    circleColorOn?: string;
-    duration?: number;
-    type?: number,
-    buttonText?: string,
-    backTextRight?: string,
-    backTextLeft?: string,
-    buttonTextStyle?: any,
-    textRightStyle?: any,
-    textLeftStyle?: any,
-    buttonStyle?: any,
-    buttonContainerStyle?: any,
-    rightContainerStyle?: any,
-    leftContainerStyle?: any
+
 }
 
-const SwitchToggle: React.SFC<Props> = (props) => {
+const Calendar: React.SFC<Props> = (props) => {
 
 };
 
-export default SwitchToggle;
+export default Calendar;
