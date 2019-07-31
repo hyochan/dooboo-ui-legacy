83bf67d0c77dfe440894821003276eb23ba55717
hyochan <dooboolab@gmail.com>
2019-07-31 22:51:33 +0900

    Add codecov to .gitignore

--- jest.config.js
+++ jest.config.js
@@ -35,7 +35,11 @@ module.exports = {
     'android.ts',
     'android.tsx',
   ],
-  // 'moduleNameMapper': {
-  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/assetsTransformer.js'
-  // },
+  modulePaths: [
+    'src',
+  ],
+  moduleNameMapper: {
+    // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|
+    //   webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/assetsTransformer.js'
+  },
 };
