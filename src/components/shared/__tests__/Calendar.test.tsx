ed9d7b23c4b5d6d10990550f20f6d4df89dc536a
hyochan <dooboolab@gmail.com>
2019-07-31 23:16:34 +0900

    Attempt ci fix #5

--- src/components/shared/__tests__/Calendar.test.tsx
+++ src/components/shared/__tests__/Calendar.test.tsx
@@ -1,5 +1,5 @@
+import 'react-native';
 import * as React from 'react';
-import { TouchableOpacity } from 'react-native';
 
 // Note: test renderer must be required after react-native.
 import Calendar from '../Calendar';
