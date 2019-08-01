e04755992d5564dff180ca72902ff2b253ff3f0e
Hyo Chan Jang <dooboolab@gmail.com>
2019-08-01 21:16:22 +0900

    Release 2.0.1 (#7)

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -2,10 +2,12 @@ import SwitchToggle from './SwitchToggle';
 import CalendarCarousel from './CalendarCarousel';
 import DropdownItem from './DropdownItem';
 import LoadingIndicator from './LoadingIndicator';
+import Button from './Button';
 
 export {
   SwitchToggle,
   CalendarCarousel,
   DropdownItem,
   LoadingIndicator,
+  Button,
 };
