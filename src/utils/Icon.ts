aaa6d3b399110a2d7a40fe976ffae82cae446ca1
hyochan <dooboolab@gmail.com>
2019-08-01 21:06:33 +0900

    Add Icon.ts again

---
+++ src/utils/Icon.ts
@@ -0,0 +1,6 @@
+export const IC_ARR_DOWN = require('../../icons/ic_arr_down.png');
+export const IC_ARR_UP = require('../../icons/ic_arr_up.png');
+export const IC_DELETE = require('../../icons/ic_delete.png');
+export const IC_EDIT = require('../../icons/ic_edit.png');
+export const IC_FACEBOOK = require('../../icons/facebook.png');
+export const IC_GOOGLE = require('../../icons/google.png');
