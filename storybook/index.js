88b89324e0d7f9058695b90c1d6cad756de2371e
Gordon Freeman <godon019@gmail.com>
2019-08-20 11:10:53 +0900

    Add storybook server

--- storybook/index.js
+++ storybook/index.js
@@ -10,7 +10,7 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({port: 6006, host: 'localhost'});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
