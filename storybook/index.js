1cdd16788c9ac9e242053b0de2325eaeb6a9a8eb
Gordon Freeman <godon019@gmail.com>
2019-09-10 18:25:42 +0900

    Updates storybook version to fix web-server-to-native connection

--- storybook/index.js
+++ storybook/index.js
@@ -11,7 +11,7 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({ port: 6006, host: 'localhost' });
+const StorybookUIRoot = getStorybookUI({ port: 7007, host: 'localhost' });
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
