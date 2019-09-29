430968df52b5431770970655381d0bd0a9d70184
hyochan <dooboolab@gmail.com>
2019-09-30 08:41:16 +0900

    Upgrade @dooboo/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,4 @@
 module.exports = {
   root: true,
   extends: '@dooboo/eslint-config',
-  rules: {
-    'max-len': [
-      'error',
-      {
-        code: 140,
-        ignoreRegExpLiterals: true,
-      },
-    ],
-  },
 };
