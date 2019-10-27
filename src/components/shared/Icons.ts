30810c387514eed81a6daddb98e5f5ae11ac763e
hyochan <dooboolab@gmail.com>
2019-10-27 16:20:55 +0900

    Fix jest

--- src/components/shared/Icons.ts
+++ src/components/shared/Icons.ts
@@ -1,10 +1,10 @@
-import icArrDown from '../../icons/ic_arr_down.png';
-import icArrUp from '../../icons/ic_arr_up.png';
-import icDelete from '../../icons/ic_delete.png';
-import icEdit from '../../icons/ic_edit.png';
-import icFacebook from '../../icons/facebook.png';
-import icGoogle from '../../icons/google.png';
-import icMagnifier from '../../icons/magnifier.png';
+import icArrDown from '../../../icons/ic_arr_down.png';
+import icArrUp from '../../../icons/ic_arr_up.png';
+import icDelete from '../../../icons/ic_delete.png';
+import icEdit from '../../../icons/ic_edit.png';
+import icFacebook from '../../../icons/facebook.png';
+import icGoogle from '../../../icons/google.png';
+import icMagnifier from '../../../icons/magnifier.png';
 
 export const IC_ARR_DOWN = icArrDown;
 export const IC_ARR_UP = icArrUp;
