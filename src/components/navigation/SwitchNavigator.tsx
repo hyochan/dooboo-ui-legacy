8d590a79f880c6bf6241803521a4ccd16bcc5e50
daheeahn <deg9810@likelion.org>
2019-11-30 12:55:26 +0900

    Set Sample Screen

--- src/components/navigation/SwitchNavigator.tsx
+++ src/components/navigation/SwitchNavigator.tsx
@@ -3,15 +3,17 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import CalendarSample from '../screen/CalendarCarouselSample';
 import EditTextSample from '../screen/EditTextSample';
 import LoadingIndicatorSample from '../screen/LoadingIndicatorSample';
+import SliderSample from '../screen/SliderSample';
 
 const SwitchNavigator = createSwitchNavigator(
   {
     CalendarSample,
     LoadingIndicatorSample,
     EditTextSample,
+    SliderSample,
   },
   {
-    initialRouteName: 'CalendarSample',
+    initialRouteName: 'SliderSample',
   },
 );
 
