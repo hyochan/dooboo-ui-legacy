b1abca0e83122d15e42298a9e32659bb74bc70b9
Hwasung Kim <marsinearth@gmail.com>
2019-12-05 02:59:43 +0900

    [Feature][Branch][Init] AutoComplete (#56)
    * add missing rect-native-gesture-handler android requirement
    
    * [Feature] Add [ThemeProvider] (#54)
    
    * Add [ThemeProvider]
    * Remove package.json in [Theme]
    
    * init autoComplete
    
    * update autoComplete for its basic functionalities
    
    * update README.md and fix minor warnings

--- src/components/navigation/SwitchNavigator.tsx
+++ src/components/navigation/SwitchNavigator.tsx
@@ -1,17 +1,19 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
+import AutoCompleteSample from '../screen/AutoCompleteSample';
 import CalendarSample from '../screen/CalendarCarouselSample';
 import EditTextSample from '../screen/EditTextSample';
 import LoadingIndicatorSample from '../screen/LoadingIndicatorSample';
 
 const SwitchNavigator = createSwitchNavigator(
   {
+    AutoCompleteSample,
     CalendarSample,
     LoadingIndicatorSample,
     EditTextSample,
   },
   {
-    initialRouteName: 'CalendarSample',
+    initialRouteName: 'AutoCompleteSample',
   },
 );
 
---
+++ src/components/shared/AutoComplete/styles.ts
@@ -0,0 +1,63 @@
+import { InputContainerProps, OptionTextProps } from './types';
+
+import { FlatList } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const InputContainer = styled.View<InputContainerProps>`
+  min-width: 250px;
+  background-color: #f3f5f7;
+  height: 60px;
+  margin: ${({ on }): string => (on ? '5px 20px' : '0 20px')};
+  margin-right: 20px;
+  border-radius: 6px;
+  flex-direction: row;
+  border: 2px solid royalblue;
+`;
+
+export const Input = styled.TextInput`
+  flex-grow: 1;
+  align-self: center;
+  font-size: 16px;
+`;
+
+export const StyledImage = styled.Image`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  right: 16px;
+`;
+
+export const CaretContainer = styled.TouchableOpacity`
+  width: 60px;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const OptionWrapper = styled.TouchableHighlight`
+  width: 100%;
+  justify-content: center;
+  height: 42px;
+  padding: 5px 20px;
+  background-color: whitesmoke;
+`;
+
+export const OptionText = styled.Text<OptionTextProps>`
+  color: #666;
+  font-weight: ${({ fontWeight }): string => fontWeight || 'bold'};
+  font-size: ${({ fontSize }): number => fontSize || 16}px;
+`;
+
+export const OptionsContainer = styled(FlatList as new () => FlatList<any>)`
+  flex-direction: column;
+  border: 1px solid #ededed;
+  border-radius: 6px;
+  margin: 0 20px 20px;
+  padding: 0 10px;
+`;
