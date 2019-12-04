32de50779974f77f4eea261ebd82adbde41ca35d
daheeahn <deg9810@likelion.org>
2019-12-04 19:28:50 +0900

    Slider in incomplete state (don't merge)

--- src/components/shared/Slider/index.tsx
+++ src/components/shared/Slider/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TouchableOpacity, View } from 'react-native';
+
 import styled from 'styled-components/native';
 
 const Container = styled.View`
@@ -14,7 +16,36 @@ interface Props {
 }
 
 function Shared(props: Props): React.ReactElement {
-  return <Container>{props.children}</Container>;
+  const [current, useCurrent] = useState(0);
+
+  return (
+    <Container>
+      <View
+        style={{
+          width: '90%',
+          height: 10,
+          backgroundColor: 'skyblue',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((s, index) => (
+            <TouchableOpacity
+              onPress={(): void => useCurrent(index)}
+              key={index}
+              style={{
+                width: current === index ? 20 : 8,
+                height: current === index ? 20 : 8,
+                borderWidth: 1,
+                borderColor: current === index ? 'skyblue' : 'transparent',
+                borderRadius: 10,
+                backgroundColor: 'white',
+              }}
+            />
+          ))}
+      </View>
+    </Container>
+  );
 }
 
 export default Shared;
