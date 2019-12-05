e10385815bf6a2e0399488d6982bf22a7b3a2465
YongPilMoon <m71661@gmail.com>
2019-12-06 00:59:38 +0900

    [Feature][Branch] Add slider storybook file, Implement basic Rail, PanResponder (#49)
    * Add slider storybook file, Implement basic Rail, gesture event handler
    
    * Seperate Rail as component
    
    * Fix lint errors
    
    * Update Slider  snapshot

---
+++ src/components/shared/Slider/Rail.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  width: 200;
+  height: 6;
+  border-radius: 3;
+  background-color: gray;
+`;
+
+const Rail: FC<{}> = () => {
+  return <Container />;
+};
+
+export default Rail;
