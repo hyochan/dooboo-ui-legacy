b2239280f88f3f01771a84ab15b868c04b04f5ad
Anthony Cho <tony3918@hotmail.com>
2019-12-06 14:05:20 +0900

    Resolve circleci build fail

--- src/components/screen/SliderSample.tsx
+++ src/components/screen/SliderSample.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from '../shared/Slider';
 
 function Page(): React.ReactElement {
-  return <Slider />;
+  return <Slider minValue={1} maxValue={5} />;
 }
 
 export default Page;
