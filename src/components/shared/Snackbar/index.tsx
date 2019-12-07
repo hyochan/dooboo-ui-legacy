5e59988f76dc643a5ba2fed148b7e2c7959f81e1
Gordon Freeman <godon019@gmail.com>
2019-12-07 22:28:16 +0900

    Add timer feature - short lasts 1500ms - long lasts 3000ms - add stories ( knobs plugin is not working properly, so I didn't use it for choosing it )

--- src/components/shared/Snackbar/index.tsx
+++ src/components/shared/Snackbar/index.tsx
@@ -7,31 +7,51 @@ const Container = styled.View`
   text-align: left;
   position: absolute;
   font-size: 16;
+  padding: 16px;
+  bottom: 50px;
+  background-color: #303235;
 `;
 
 export interface SnackbarProps {
   text: string;
   testID?: string;
+  show: boolean;
+  setShow: (show: boolean) => void;
+  timer?: Timer;
+}
+
+export enum Timer {
+  SHORT = 1500,
+  LONG = 3000,
 }
 
 const Snackbar: React.FC<SnackbarProps> = (props) => {
+  const { testID, show, setShow, timer = Timer.SHORT } = props;
+  console.log('timer', timer);
+  React.useEffect(() => {
+    let timeout;
+    if (show === true) {
+      timeout = setTimeout(() => {
+        setShow(false);
+      }, timer);
+    }
+    return (): void => clearTimeout(timeout);
+  }, [show]);
+
   return (
-    <Container
-      testID={props.testID}
-      style={{
-        padding: 16,
-        bottom: 50,
-        backgroundColor: '#303235',
-      }}
-    >
-      <Text
-        style={{
-          color: '#ffffff',
-        }}
-      >
-        {props.text}
-      </Text>
-    </Container>
+    <>
+      {show && (
+        <Container testID={testID}>
+          <Text
+            style={{
+              color: '#ffffff',
+            }}
+          >
+            {props.text}
+          </Text>
+        </Container>
+      )}
+    </>
   );
 };
 
