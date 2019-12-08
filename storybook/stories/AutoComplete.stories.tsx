43fe8d50373dfb7e7d6ed2456c8ded638722a19d
Mars <marsinearth@gmail.com>
2019-12-09 01:13:24 +0900

    revert STORYBOOK_START back to true and modify wrapper style for autoComplete storybook

--- storybook/stories/AutoComplete.stories.tsx
+++ storybook/stories/AutoComplete.stories.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { number, object, text } from '@storybook/addon-knobs';
 
 import AutoComplete from '../../src/components/shared/AutoComplete';
+import { Platform } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import styled from 'styled-components/native';
 
@@ -10,18 +11,24 @@ export const ContainerDeco = (storyFn): React.ReactElement => (
 );
 
 const Wrapper = styled.SafeAreaView`
+  flex: 1;
+  background-color: transparent;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: ${Platform.OS === 'ios' ? 30 : 15}px;
 `;
 
 storiesOf('AutoComplete', module)
   .addDecorator(ContainerDeco)
-  .add('default', () => (<Default />));
+  .add('default', () => <Default />);
 
 function Default(): React.ReactElement {
   const [value] = useState<string>();
 
   return (
     // if Wrapper Style height is 50?
-    <Wrapper style={object('wrapperStyle', { height: 300 }) }>
+    <Wrapper>
       <AutoComplete
         value={value}
         style={object('inputStyle', { backgroundColor: '#f3f5f7' })}
