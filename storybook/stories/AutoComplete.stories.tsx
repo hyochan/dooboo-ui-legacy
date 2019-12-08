e833d151f8688781f041c1af0b2da1756f9b4ad9
cwalker <jb9229@gmail.com>
2019-12-09 00:24:41 +0900

    Add AutoComplete C. Story (#67)
    * Add AutoComplete C. Story
      - add addon-ondevice-knobs D.
    
    * Update Doc/AutoComplete/ Add Storybook in Readme
    
    * Fix/AutoComplete/ lint error fixed
    
    * Update/AutoComplete Storybook/ Wrapper Style
    
      - Add Wrapper Style change to Object from height number

---
+++ storybook/stories/AutoComplete.stories.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { number, object, text } from '@storybook/addon-knobs';
+
+import AutoComplete from '../../src/components/shared/AutoComplete';
+import { storiesOf } from '@storybook/react-native';
+import styled from 'styled-components/native';
+
+export const ContainerDeco = (storyFn): React.ReactElement => (
+  <Wrapper>{storyFn()}</Wrapper>
+);
+
+const Wrapper = styled.SafeAreaView`
+`;
+
+storiesOf('AutoComplete', module)
+  .addDecorator(ContainerDeco)
+  .add('default', () => (<Default />));
+
+function Default(): React.ReactElement {
+  const [value] = useState<string>();
+
+  return (
+    // if Wrapper Style height is 50?
+    <Wrapper style={object('wrapperStyle', { height: 300 }) }>
+      <AutoComplete
+        value={value}
+        style={object('inputStyle', { backgroundColor: '#f3f5f7' })}
+        debounceDelay={number('debounceDelay', 400)}
+        placeholderText={text('placeholder', 'Choose a country')}
+      />
+    </Wrapper>
+  );
+}
