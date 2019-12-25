56d7057d498823f153233d1de104131aadaad9f0
Terry <jtchoi@aitrics.com>
2019-12-25 21:00:46 +0900

    ✅ add some test

--- src/components/shared/__tests__/Snackbar.test.tsx
+++ src/components/shared/__tests__/Snackbar.test.tsx
@@ -1,41 +1,47 @@
-import 'react-native';
-
 import * as React from 'react';
 
-import Snackbar, { SnackbarProps } from '../Snackbar';
+import Snackbar, { SnackbarProps, SnackbarRef } from '../Snackbar';
+import { Text, TouchableOpacity, View } from 'react-native';
 
-import { render } from '@testing-library/react-native';
-import renderer from 'react-test-renderer';
+import { render, waitForElement, fireEvent } from '@testing-library/react-native';
+import renderer, { act } from 'react-test-renderer';
 
 // Note: test renderer must be required after react-native.
-
-const props: SnackbarProps = {
-  testID: 'snackbar',
-  text: 'SnackBar!',
-  show: true,
-  setShow: jest.fn(),
-};
-let component: React.ReactElement;
+let snackbarRef: React.MutableRefObject<SnackbarRef>;
 
 jest.useFakeTimers();
 
+function TestWrapper(): React.ReactElement {
+  snackbarRef = React.useRef();
+  return (
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity
+        testID="Button"
+        onPress={(): void => snackbarRef.current && snackbarRef.current.show({
+          text: 'snackbar content',
+        })}>
+        <Text>
+          show snack bar
+        </Text>
+      </TouchableOpacity>
+      <Snackbar ref={snackbarRef} />
+    </View>
+  );
+}
+
 describe('[Snackbar]', () => {
-  beforeEach(() => {
-    component = <Snackbar {...props} />;
-  });
-
-  it('renders without crashing', () => {
-    const rendered = renderer.create(component);
+  it('renders TestWrapper without crashing', () => {
+    const rendered = renderer.create(<TestWrapper />);
     expect(rendered.toJSON()).toMatchSnapshot();
-    expect(rendered.toJSON()).toBeTruthy();
-
-    rendered.update(<Snackbar {...{ ...props, show: false }} />);
-    expect(rendered.toJSON()).toBeFalsy();
   });
 
-  it('should dismiss after timeout', async () => {
-    render(component);
+  it('should simulate showing snackbar', async () => {
+    const renderResult = render(<TestWrapper />);
+    const btn = renderResult.getByTestId('Button');
+    act(() => {
+      fireEvent.press(btn);
+      fireEvent.press(btn);
+    });
     jest.runAllTimers();
-    expect(props.setShow).toBeCalled();
   });
 });
