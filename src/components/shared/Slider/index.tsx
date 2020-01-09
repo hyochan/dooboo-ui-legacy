d902b162fdce6946b7259b6816adfce6b3ef9936
YongPilMoon <m71661@gmail.com>
2020-01-09 20:49:59 +0900

    [Feature][Branch][Slider] Implement Thumb (#78)
    * Implement Thumb
    
    * Add basic Thumb
    
    * Change slider variable name for readability
    
    percentagePosition -> percent
    layoutX -> sliderPositionX
    sliderWidth -> sliderLength
    
    * Implement onChange function, default Value
    
    refactor: seperate util function
    
    * Change variable name: sliderLength -> sliderWidth
    
    Co-authored-by: nick-prnd <46916598+nick-prnd@users.noreply.github.com>

--- src/components/shared/Slider/index.tsx
+++ src/components/shared/Slider/index.tsx
@@ -1,9 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useState } from 'react';
+import { getPercent, percentToValue, valueToPercent } from './utils';
 import { PanResponder } from 'react-native';
 import Rail from './Rail';
+import Thumb from './Thumb';
 import styled from 'styled-components/native';
 
-const Container = styled.View``;
+const Container = styled.View`
+  position: relative;
+`;
 
 interface Props {
   defaultValue?: number;
@@ -13,21 +17,51 @@ interface Props {
   step?: number;
 }
 
-const Slider: FC<Props> = () => {
+const Slider: FC<Props> = ({
+  maxValue = 100,
+  minValue = 0,
+  defaultValue,
+  onChange,
+}) => {
+  const sliderRef = useRef<any>();
+  const [sliderWidth, setSliderWidth] = useState<number>(0);
+  const [sliderPositionX, setSliderPositionX] = useState(0);
+  const [percent, setPercent] = useState(
+    defaultValue ? valueToPercent(defaultValue, maxValue, minValue) : 0,
+  );
+
   const panResponder = React.useMemo(
     () =>
       PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-        onPanResponderGrant: (evt, gestureState) => {
-          console.log(gestureState);
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderMove: (evt, gestureState) => {
+          // the latest screen coordinates of the recently-moved touch
+          const moveX = gestureState.moveX;
+          const percent = getPercent(moveX - sliderPositionX, sliderWidth);
+          setPercent(percent);
+          if (onChange) {
+            onChange(percentToValue(percent, maxValue, minValue));
+          }
         },
       }),
-    [],
+    [sliderPositionX, sliderWidth, onChange],
   );
 
   return (
-    <Container {...panResponder.panHandlers}>
+    <Container
+      ref={sliderRef}
+      {...panResponder.panHandlers}
+      onLayout={(): void => {
+        if (sliderRef) {
+          sliderRef.current.measure((x, y, width, height, pageX) => {
+            setSliderPositionX(pageX);
+            setSliderWidth(width);
+          });
+        }
+      }}
+    >
       <Rail />
+      <Thumb percent={percent} />
     </Container>
   );
 };
