afc85be417275e06e8cdbe18bc538f2dca70f0d3
Eunho Lee <55861805+devohno@users.noreply.github.com>
2020-01-21 23:30:35 +0900

    [Feature][TinderCard] Initial implement for tinder-card component (#94)
    * Expose handleCancel method instead putting inside the component
    * Enhance typings
    * Upgrade packages
    * Update default styling in [EditText]
    
    Co-authored-by: Hyo Chan Jang <dooboolab@gmail.com>

---
+++ src/components/shared/TinderCard/index.tsx
@@ -0,0 +1,248 @@
+import {
+  Animated,
+  Dimensions,
+  LayoutAnimation,
+  PanResponder,
+  Text,
+  UIManager,
+  View,
+} from 'react-native';
+import React, {
+  ReactElement,
+  RefForwardingComponent,
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useMemo,
+  useState,
+} from 'react';
+
+import styled from 'styled-components/native';
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
+
+export enum TinderCardDirection {
+  RIGHT = 'right',
+  LEFT = 'left'
+}
+
+export interface TinderCardRef {
+  handleCancel: () => void;
+  forceSwipe: (direction: TinderCardDirection) => void;
+}
+
+interface Props {
+  testID?: string;
+  onSwipeRight?: (item: any) => void;
+  onSwipeLeft?: (item: any) => void;
+  onCancel?: () => void;
+  data: any[];
+  renderCards: (item: any, type?: number) => ReactElement;
+  renderNoMoreCards: () => ReactElement;
+  renderCardLabel?: (type: number) => ReactElement;
+  rotate?: boolean;
+  stackSize?: number;
+}
+
+const Container = styled.View`
+  width: ${SCREEN_WIDTH};
+  align-items: center;
+`;
+
+const TextArea = styled.View`
+  width: ${SCREEN_WIDTH};
+  height: 100%;
+  z-index: 99;
+  position: absolute;
+  top: 20;
+`;
+
+const NoCard = styled.View`
+  width: 100%;
+  height: 200px;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+const TinderCard: RefForwardingComponent<TinderCardRef, Props> = (
+  props,
+  ref,
+): ReactElement => {
+  const [cardIndex, setCardIndex] = useState(0);
+  const [type, setType] = useState(0);
+  const position = useMemo(() => new Animated.ValueXY(), []);
+
+  const resetPosition = (): void => {
+    setType(0);
+    Animated.spring(position, {
+      toValue: { x: 0, y: 0 },
+    }).start();
+  };
+
+  const onSwipeCompleted = useCallback((direction: TinderCardDirection): void => {
+    const { onSwipeLeft, onSwipeRight, data } = props;
+
+    position.setValue({ x: 0, y: 0 });
+
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
+
+    let currentIndex = 0;
+
+    setCardIndex((idx) => {
+      currentIndex = idx;
+      return idx + 1;
+    });
+
+    if (direction === TinderCardDirection.RIGHT) {
+      onSwipeRight && onSwipeRight(data[currentIndex]);
+    } else {
+      onSwipeLeft && onSwipeLeft(data[currentIndex]);
+    }
+  }, []);
+
+  const forceSwipe = (direction: TinderCardDirection): void => {
+    const x = direction === TinderCardDirection.RIGHT ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION,
+    }).start(() => {
+      onSwipeCompleted(direction);
+      setType(0);
+    });
+  };
+
+  const _panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (evt, gestureState) => {
+          if (gestureState.dx > 0) {
+            setType(2);
+          } else if (gestureState.dx < 0) {
+            setType(1);
+          }
+          position.setValue({ x: gestureState.dx, y: gestureState.dy });
+        },
+        onPanResponderRelease: (evt, gesture) => {
+          if (gesture.dx > SWIPE_THRESHOLD) {
+            forceSwipe(TinderCardDirection.RIGHT);
+          } else if (gesture.dx < -SWIPE_THRESHOLD) {
+            forceSwipe(TinderCardDirection.LEFT);
+          } else {
+            resetPosition();
+          }
+        },
+      }),
+    [],
+  );
+
+  const getCardStyle = (): {} => {
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-45deg', '0deg', '45deg'],
+    });
+
+    const rotateStyle = props.rotate ? { transform: [{ rotate }] } : {};
+
+    return {
+      ...position.getLayout(),
+      ...rotateStyle,
+    };
+  };
+
+  const _renderCards = (): ReactElement | (ReactElement | null)[] => {
+    if (!props.data || cardIndex >= props.data.length) {
+      if (props.renderNoMoreCards) {
+        return props.renderNoMoreCards();
+      } else {
+        return <View/>;
+      }
+    }
+
+    const dataSet = props.data.map((item, idx) => {
+      if (idx < cardIndex) return null;
+      if (idx === cardIndex) {
+        return (
+          <Animated.View
+            key={`card__${idx}`}
+            style={[
+              getCardStyle(),
+              {
+                position: 'absolute',
+                top: 0,
+                width: SCREEN_WIDTH,
+                zIndex: 99,
+              },
+            ]}
+            {..._panResponder.panHandlers}>
+            {props.renderCards(item, type)}
+            <TextArea>
+              {props.renderCardLabel && props.renderCardLabel(type)}
+            </TextArea>
+          </Animated.View>
+        );
+      }
+
+      const indexGap = idx - cardIndex;
+      const stackSize = props.stackSize || 3;
+      const behindHeight =
+        indexGap <= stackSize ? 10 * indexGap : 10 * stackSize;
+
+      return (
+        <Animated.View
+          key={`card__${idx}`}
+          style={[
+            {
+              position: 'absolute',
+              width: SCREEN_WIDTH - 10 * indexGap,
+              top: behindHeight,
+              zIndex: 5,
+            },
+          ]}>
+          {props.renderCards(item)}
+        </Animated.View>
+      );
+    });
+    return dataSet.reverse();
+  };
+
+  const handleCancel = (): void => {
+    if (cardIndex > 0 && props.onCancel) {
+      setCardIndex((index) => index - 1);
+      props.onCancel();
+    }
+  };
+
+  useImperativeHandle(ref, () => ({
+    forceSwipe,
+    handleCancel,
+  }));
+
+  return (
+    <Container>
+      {_renderCards()}
+    </Container>
+  );
+};
+
+const _renderNoMoreCards = (): ReactElement => (
+  <NoCard>
+    <Text>No more cards</Text>
+  </NoCard>
+);
+
+TinderCard.defaultProps = {
+  onSwipeRight: (): void => {},
+  onSwipeLeft: (): void => {},
+  onCancel: (): void => {},
+  renderNoMoreCards: _renderNoMoreCards,
+  rotate: false,
+  stackSize: 3,
+};
+
+export default forwardRef(TinderCard);
---
+++ storybook/stories/TinderCard.stories.tsx
@@ -0,0 +1,221 @@
+import { Dimensions, ImageSourcePropType, Text, TouchableOpacity, View } from 'react-native';
+import React, {
+  ReactElement,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import TinderCard, { TinderCardDirection, TinderCardRef } from '../../src/components/shared/TinderCard';
+
+import { ContainerDeco } from '../decorators';
+import { storiesOf } from '@storybook/react-native';
+import styled from 'styled-components/native';
+import { tinderCardDummyData } from './asset/dummyData/data';
+import useInterval from 'react-useinterval';
+
+const INNER_HEIGHT = Dimensions.get('screen').height;
+
+storiesOf('TinderCard', module)
+  .addDecorator(ContainerDeco)
+  .add('default', () => (
+    <>
+      <Default />
+    </>
+  ));
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  margin-top: ${INNER_HEIGHT * 0.1};
+`;
+
+const HeadContainer = styled.View`
+  width: 250px;
+  height: 50px;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  background-color: lightgrey;
+  border-radius: 5px;
+`;
+
+const StyledText = styled.Text`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const CardContainer = styled.View`
+  flex: 1;
+  min-height: ${INNER_HEIGHT * 0.5};
+  margin: 10%;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgrey;
+  border-width: 2px;
+  border-color: white;
+  border-radius: 5px;
+  position: relative;
+`;
+
+const IndexText = styled.Text`
+  position: absolute;
+  color: white;
+  font-size: 32;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 5px;
+  z-index: 1;
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: black;
+  opacity: 0.7;
+`;
+
+const BannerImage = styled.Image`
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+`;
+
+const CardLabel = styled.Text`
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  bottom: 50;
+  font-size: 35;
+  font-weight: 600;
+  color: lightcyan;
+  position: absolute;
+  top: 50;
+`;
+
+const NoCard = styled.View`
+  width: 100%;
+  height: 200px;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+interface Item {
+  id: string;
+  image: ImageSourcePropType;
+}
+
+function Default(): React.ReactElement {
+  const tinderCard = useRef<TinderCardRef>(null);
+  const [like, setLike] = useState(0);
+  const [unlike, setUnlike] = useState(0);
+  const [data, setData] = useState(tinderCardDummyData);
+
+  useInterval(() => {
+    if (data.length > 20) return;
+    setData([...data, ...tinderCardDummyData]);
+  }, 5000);
+
+  const actionStack = useMemo(() => [], []);
+
+  const handleUnlike = (): void => {
+    setUnlike((unlike) => unlike + 1);
+    actionStack.push('unlike');
+  };
+
+  const handleLike = (): void => {
+    setLike((like) => like + 1);
+    actionStack.push('like');
+  };
+
+  const handleCancel = (): void => {
+    if (actionStack.pop() === 'like') {
+      setLike((value) => value - 1);
+    } else {
+      setUnlike((value) => value - 1);
+    }
+  };
+
+  const _renderCards = (item: Item): ReactElement => {
+    return (
+      <CardContainer>
+        <IndexText>{item.id}</IndexText>
+        <BannerImage source={item.image} resizeMode="cover" />
+      </CardContainer>
+    );
+  };
+
+  const _renderCardLabel = (type: number): ReactElement => {
+    if (type === 1) {
+      return <CardLabel>Like</CardLabel>;
+    } else if (type === 2) {
+      return <CardLabel>Unike</CardLabel>;
+    } else return null;
+  };
+
+  const _renderNoMoreCards = (): ReactElement => (
+    <NoCard>
+      <StyledText>No more cards</StyledText>
+    </NoCard>
+  );
+
+  return (
+    <Container>
+      <HeadContainer>
+        <StyledText style={{ color: 'red' }}>Like: {like}</StyledText>
+        <StyledText style={{ color: 'blue' }}>Unlike: {unlike}</StyledText>
+      </HeadContainer>
+      <View
+        style={{
+          width: '100%',
+          height: 600,
+          alignItems: 'center',
+        }}
+      >
+        <TinderCard
+          testID="tinderCard"
+          ref={tinderCard}
+          onSwipeRight={handleUnlike}
+          onSwipeLeft={handleLike}
+          onCancel={handleCancel}
+          data={data}
+          renderCards={_renderCards}
+          renderNoMoreCards={_renderNoMoreCards}
+          rotate
+          stackSize={0}
+        />
+        <View style={{
+          flexDirection: 'row',
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          paddingHorizontal: 80,
+          justifyContent: 'space-between',
+        }}>
+          <TouchableOpacity
+            style={{
+              padding: 8,
+            }}
+            onPress={(): void => {
+              tinderCard.current.forceSwipe(TinderCardDirection.LEFT);
+            }}
+          ><Text>LEFT</Text></TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              padding: 8,
+            }}
+            onPress={(): void => {
+              tinderCard.current.handleCancel();
+            }}
+          ><Text>UNDO</Text></TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              padding: 8,
+            }}
+            onPress={(): void => {
+              tinderCard.current.forceSwipe(TinderCardDirection.RIGHT);
+            }}
+          ><Text>RIGHT</Text></TouchableOpacity>
+        </View>
+      </View>
+    </Container>
+  );
+}
