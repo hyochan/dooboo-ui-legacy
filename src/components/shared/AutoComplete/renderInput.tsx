743fccef9bcfe08a6229b677b656fa4fb06be092
seonghyeon <impressor7@gmail.com>
2020-01-24 02:26:28 +0900

    Add: AutoComplete renderInput animation state relating to value's exsistence

--- src/components/shared/AutoComplete/renderInput.tsx
+++ src/components/shared/AutoComplete/renderInput.tsx
@@ -20,12 +20,13 @@ const RenderInput =
     const [animatedIsFocused] = useState(new Animated.Value(on ? 1 : 0));
 
     useEffect(() => {
-      Animated.timing(animatedIsFocused, {
-        toValue: on ? 1 : 0,
-        delay: 300,
-        duration: 200,
-      }).start();
-    }, [on]);
+      const config = {
+        toValue: on ? 1 : value ? 2 : 0,
+        delay: value ? 0 : 300,
+        duration: value ? 0 : 200,
+      };
+      Animated.timing(animatedIsFocused, config).start();
+    }, [on, value]);
 
     return (
       <InputInnerContainer>
@@ -35,26 +36,26 @@ const RenderInput =
             paddingRight: 4,
             position: 'absolute',
             top: animatedIsFocused.interpolate({
-              inputRange: [0, 1],
-              outputRange: [17, -10],
+              inputRange: [0, 1, 2],
+              outputRange: [17, -10, -10],
             }),
             left: animatedIsFocused.interpolate({
-              inputRange: [0, 1],
-              outputRange: [7, 10],
+              inputRange: [0, 1, 2],
+              outputRange: [7, 10, 10],
             }),
             fontSize: animatedIsFocused.interpolate({
-              inputRange: [0, 1],
-              outputRange: [20, 14],
+              inputRange: [0, 1, 2],
+              outputRange: [20, 14, 14],
             }),
             color: animatedIsFocused.interpolate({
-              inputRange: [0, 1],
-              outputRange: ['#cdd2d7', 'royalblue'],
+              inputRange: [0, 1, 2],
+              outputRange: ['#cdd2d7', 'royalblue', '#000000'],
             }),
             backgroundColor: 'white',
             marginBottom: 2,
           }}
         >
-          {!value && placeholderLabel}
+          {placeholderLabel}
         </Animated.Text>
         <Input {...props} onFocus={onFocus} onBlur={onBlur} ref={ref as any} />
         {!!value && (
