2714afa4d83984ece3d4b45d161f97a601508376
Sandwichj <dreamingsandwich@gmail.com>
2020-01-29 04:04:21 +0900

    Add [Slider] props.

--- src/components/shared/Slider/Thumb.tsx
+++ src/components/shared/Slider/Thumb.tsx
@@ -3,6 +3,15 @@ import React, { FC } from 'react';
 
 import styled from 'styled-components/native';
 
+interface ThumbPositionerType {
+  percent: number;
+}
+
+const ThumbPositioner = styled.View<ThumbPositionerType>`
+  position: absolute;
+  left: ${({ percent }): string => `${percent}%`};
+`;
+
 const StyledThumb = styled.View`
   width: 12;
   height: 12;
@@ -13,37 +22,45 @@ const StyledThumb = styled.View`
 
 interface Props {
   testID?: string;
+  percent: number;
   size?: number;
-  opacityValue: Animated.Value;
-  scaleValue: Animated.Value;
+  scaleValue?: Animated.Value;
+  opacityValue?: Animated.Value;
+  customThumb?: React.ReactElement;
   style?: StyleProp<ViewStyle>;
 }
 
 const Thumb: FC<Props> = ({
   testID,
+  percent,
   size = 12,
-  scaleValue,
-  opacityValue,
+  scaleValue = new Animated.Value(0.01),
+  opacityValue = new Animated.Value(0.12),
+  customThumb,
   style,
 }) => {
   const rippleSize = size * 2;
 
   return (
-    <StyledThumb testID={testID} style={style}>
-      <Animated.View
-        style={{
-          position: 'absolute',
-          top: -6,
-          left: -6,
-          width: rippleSize,
-          height: rippleSize,
-          borderRadius: rippleSize / 2,
-          transform: [{ scale: scaleValue }],
-          opacity: opacityValue,
-          backgroundColor: 'black',
-        }}
-      />
-    </StyledThumb>
+    <ThumbPositioner testID="thumb-positioner-test-id" percent={percent}>
+      {customThumb || (
+        <StyledThumb testID={testID} style={style}>
+          <Animated.View
+            style={{
+              position: 'absolute',
+              top: -6,
+              left: -6,
+              width: rippleSize,
+              height: rippleSize,
+              borderRadius: rippleSize / 2,
+              transform: [{ scale: scaleValue }],
+              opacity: opacityValue,
+              backgroundColor: '#0b21e8',
+            }}
+          />
+        </StyledThumb>
+      )}
+    </ThumbPositioner>
   );
 };
 
