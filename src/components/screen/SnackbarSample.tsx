88b30428d68ba51f6ab136f3981909ce3f1ab4c5
Terry <jtchoi@aitrics.com>
2020-02-25 00:22:54 +0900

    Remove unused sample of Snackbar (to pass ci)

--- src/components/screen/SnackbarSample.tsx
+++ src/components/screen/SnackbarSample.tsx
@@ -1,6 +1,6 @@
 import { Alert, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
-import React, { useRef } from 'react';
-import Snackbar, { SnackbarProvider, SnackbarRef, useSnackbarContext } from '../shared/Snackbar';
+import { SnackbarProvider, useSnackbarContext } from '../shared/Snackbar';
+import React from 'react';
 
 function Container(): React.ReactElement {
   const snackbar = useSnackbarContext();
@@ -28,25 +28,6 @@ function Provider(): React.ReactElement {
       <SnackbarProvider>
         <Container/>
       </SnackbarProvider>
-    </SafeAreaView>
-  );
-}
-
-function Page(): React.ReactElement {
-  const snackbar = useRef<SnackbarRef>(null);
-  const onPress = (): void => {
-    snackbar.current && snackbar.current.show({
-      text: 'Simple Snackbar is opened',
-    });
-  };
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <TouchableOpacity onPress={onPress} style={{ borderWidth: 1, padding: 10 }}>
-          <Text>Show Snackbar</Text>
-        </TouchableOpacity>
-        <Snackbar ref={snackbar}/>
-      </View>
     </SafeAreaView>
   );
 }
