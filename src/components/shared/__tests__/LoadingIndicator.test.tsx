076006cb3c1a69a321bf8410205f7b7c646a06ea
Jeff Gu Kang <jeffgukang@gmail.com>
2020-05-26 15:06:12 +0900

    [Feature] Rating component (#192)
    **Is your feature request related to a problem? Please describe.**
    
    New Rating component related to #37.
    
    **Describe the solution you'd like**
    
    Ref: https://material-ui.com/components/rating

--- src/components/shared/__tests__/LoadingIndicator.test.tsx
+++ src/components/shared/__tests__/LoadingIndicator.test.tsx
@@ -118,4 +118,81 @@ describe('[LoadingIndicator] render', () => {
     expect(rendered).toMatchSnapshot();
     expect(rendered).toBeTruthy();
   });
+
+  it('renders with [imgSource]', () => {
+    props = createTestProps({
+      imgSource: 'http',
+    });
+    component = <LoadingIndicator {...props} />;
+    const rendered: renderer.ReactTestRendererJSON = renderer
+      .create(component)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('renders with size=small', () => {
+    props = createTestProps({
+      imgSource: 'http',
+      size: 'small',
+    });
+    component = <LoadingIndicator {...props} />;
+    const rendered: renderer.ReactTestRendererJSON = renderer
+      .create(component)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('renders with size=undefined', () => {
+    props = createTestProps({
+      imgSource: 'http',
+      size: undefined,
+    });
+    component = <LoadingIndicator {...props} />;
+    const rendered: renderer.ReactTestRendererJSON = renderer
+      .create(component)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('renders with !size with none string imgSource', () => {
+    props = createTestProps({
+      imgSource: 10,
+      size: null,
+    });
+    component = <LoadingIndicator {...props} />;
+    const rendered: renderer.ReactTestRendererJSON = renderer
+      .create(component)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('renders with !size with string imgSource', () => {
+    props = createTestProps({
+      imgSource: 'test',
+      size: null,
+    });
+    component = <LoadingIndicator {...props} />;
+    const rendered: renderer.ReactTestRendererJSON = renderer
+      .create(component)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+
+  it('renders with size=test', () => {
+    props = createTestProps({
+      imgSource: 10,
+      size: 'test',
+    });
+    component = <LoadingIndicator {...props} />;
+    const rendered: renderer.ReactTestRendererJSON = renderer
+      .create(component)
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
 });
---
+++ src/components/shared/__tests__/Rating.test.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+
+import Rating from '../Rating';
+// Note: test renderer must be required after react-native.
+import { TouchableOpacity } from 'react-native';
+import renderer from 'react-test-renderer';
+
+let props: any;
+
+// let testingLib: RenderResult;
+
+const component = (props?): React.ReactElement => {
+  return <Rating {...props} />;
+};
+
+const createTestProps = (obj: object): object => ({
+  navigation: {
+    navigate: jest.fn(),
+  },
+  ...obj,
+});
+
+describe('[Rating] render', () => {
+  beforeEach(() => {
+    props = createTestProps({});
+  });
+
+  it('renders without crashing', () => {
+    const rendered: renderer.ReactTestRendererJSON | null = renderer
+      .create(component())
+      .toJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+
+  describe('[Rating] Interaction', (): void => {
+    it('should simulate onPress', (): void => {
+      const handlePress = jest.fn();
+      const rendered = renderer.create(
+        component({
+          testID: 'RATING_ID',
+          onChange: handlePress,
+        }),
+        {
+          createNodeMock: (element) => {
+            return {
+              handlePressIn: handlePress,
+              handlePressOut: handlePress,
+            };
+          },
+        },
+      );
+      const ratingClick = rendered.root.findByType(TouchableOpacity);
+      renderer.act(() => {
+        ratingClick.props.onPress();
+      });
+      expect(handlePress).toHaveBeenCalled();
+    });
+    it('should simulate props', (): void => {
+      const rendered = renderer.create(
+        component({
+          testID: 'RATING_ID',
+        }),
+      );
+
+      rendered.update(component({ value: 1, index: 0 }));
+      expect(rendered).toMatchSnapshot();
+      expect(rendered).toBeTruthy();
+
+      rendered.update(component({ disabled: true }));
+      expect(rendered).toMatchSnapshot();
+      expect(rendered).toBeTruthy();
+    });
+  });
+});
