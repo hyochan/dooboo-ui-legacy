fa3385a8c1bcd5097026ff854112f8c5b741897f
DevYuns <ydhun93@gmail.com>
2020-06-11 16:51:51 +0900

    Accordion enhancement (#201)
    Accordion enhancement
    - Make it available both web & mobile
    - Remove UseNativeDriver for error
    - Add property: isAnimated
    
    Minor Upgrades
    - animation change(spring->timing)
    
    Co-authored-by: devethan <devohno.ko@gmail.com>
    Co-authored-by: Yunsdev <yunsdev>

--- stories/dooboo-ui/Accordion.stories.tsx
+++ stories/dooboo-ui/Accordion.stories.tsx
@@ -1,24 +1,41 @@
 import { IC_ARR_DOWN, IC_ARR_UP } from '../Icon';
 import React, { ReactElement } from 'react';
-import { ScrollView, Text, View } from 'react-native';
-
 import Accordion from '../../main/Accordion';
 import { ContainerDeco } from '../../storybook/decorators';
+import { Text } from 'react-native';
+
 import { storiesOf } from '@storybook/react-native';
 import styled from 'styled-components/native';
 
 const Container = styled.SafeAreaView`
   flex: 1;
-  background-color: transparent;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+const AccordionContainer = styled.View`
+  background-color: transparent;
+  flex-direction: column;
+  margin: 0 20px;
+  width: 280px;
+`;
+
+const CustomHeaderContainer = styled.View`
+  padding: 10px;
+  background-color: darkgrey;
+`;
+
+const CustomBodyContainer = styled.View`
+  padding: 10px;
+  background-color: #ddd;
 `;
 
 const StyledImage = styled.Image`
   width: 20px;
   height: 20px;
   position: absolute;
+  top: 8px;
   right: 16px;
 `;
 
@@ -29,56 +46,60 @@ const Default = (): React.ReactElement => {
       body: 'Hi. I love this component. What do you think?',
     },
     {
-      title: 'See this one too',
-      body: 'Yes. You can have more items.',
+      title: 'Title 2',
+      body: 'Hi. I love this component. What do you think?',
     },
     {
-      title: 'Thrid thing',
-      body: `What about very long text? What about very long text?
-        What about very long text? What about very long text?
-        What about very long text? What about very long text?
-        What about very long text? What about very long text?
-        What about very long text? What about very long text?
-        What about very long text? What about very long text?`,
+      title: 'Title 3',
+      body: 'Hi. I love this component. What do you think?',
+    },
+    {
+      title: 'Title 4',
+      body: 'Hi. I love this component. What do you think?',
+    },
+    {
+      title: 'Title 5',
+      body: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
     },
   ];
 
   return (
     <Container>
-      <ScrollView style={{ alignSelf: 'stretch', paddingHorizontal: 20 }}>
+      <AccordionContainer>
         {contents.map((param, i) => {
           return (
             <Accordion
               key={i}
+              isAnimated={true}
               contentVisible={false}
-              visibleElement={
-                <StyledImage
-                  source={IC_ARR_UP}
-                />
-              }
-              invisibleElement={
-                <StyledImage
-                  source={IC_ARR_DOWN}
-                />
-              }
+              duration={300}
+              visibleElement={<StyledImage source={IC_ARR_UP} />}
+              invisibleElement={<StyledImage source={IC_ARR_DOWN} />}
               header={
-                <View style={{ height: 28 }}>
+                <CustomHeaderContainer>
                   <Text
                     style={{
                       fontSize: 16,
-                      color: 'blue',
-                    }}
-                  >
+                      fontWeight: '600',
+                      color: '#fff',
+                    }}>
                     {param.title}
                   </Text>
-                </View>
-              }
-            >
-              <Text style={{ fontSize: 20 }}>{param.body}</Text>
+                </CustomHeaderContainer>
+              }>
+              <CustomBodyContainer>
+                <Text>{param.body}</Text>
+              </CustomBodyContainer>
             </Accordion>
           );
         })}
-      </ScrollView>
+      </AccordionContainer>
     </Container>
   );
 };
