5fc5fd7560002a37f328e93c4e1d08af5ef43959
SONGMINSEOK <somony9292@gmail.com>
2020-07-21 20:05:43 +0900

    [Enhance][Theme] Color @dooboo/theme  colors enhancement (#249)
    * @dooboo/theme  colors enhancement
    
    * Remove unnecessary labelInput

--- packages/theme/index.tsx
+++ packages/theme/index.tsx
@@ -1,4 +1,7 @@
-import { DefaultTheme, ThemeProvider as OriginalThemeProvider } from 'styled-components/native';
+import {
+  DefaultTheme,
+  ThemeProvider as OriginalThemeProvider,
+} from 'styled-components/native';
 import {
   Theme as DoobooTheme,
   ThemeParam,
@@ -8,11 +11,13 @@ import {
   light as lightTheme,
 } from './theme';
 import React, { useState } from 'react';
+
 import createCtx from './createCtx';
 
 interface Context {
   themeType: ThemeType;
   theme: DefaultTheme;
+  doobooTheme: DoobooTheme;
   changeThemeType: () => void;
 }
 const [useCtx, Provider] = createCtx<Context>();
@@ -24,12 +29,14 @@ interface Props {
   // using initial ThemeType is essential while testing apps with consistent ThemeType
   initialThemeType?: ThemeType;
   customTheme?: ThemeParam;
+  doobooTheme?: DoobooTheme;
 }
 
 function ThemeProvider({
   children,
   initialThemeType = defaultThemeType,
   customTheme,
+  doobooTheme,
 }: Props): React.ReactElement {
   const [themeType, setThemeType] = useState(initialThemeType);
 
@@ -67,8 +74,8 @@ function ThemeProvider({
         themeType,
         changeThemeType,
         theme: theme,
-      }}
-    >
+        doobooTheme: doobooTheme,
+      }}>
       <OriginalThemeProvider theme={theme}>{children}</OriginalThemeProvider>
     </Provider>
   );
