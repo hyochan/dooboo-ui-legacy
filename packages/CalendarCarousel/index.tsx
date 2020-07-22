433a219a5941b365649ae091821aadda983ac6a4
shl_jessie <jessielee.shl@gmail.com>
2020-07-22 18:16:54 +0900

    load calendar of prev, current, and next month inside of scrollView, create a renderCalendar function that will render calendars as they load

--- packages/CalendarCarousel/index.tsx
+++ packages/CalendarCarousel/index.tsx
@@ -123,13 +123,6 @@ const styles = StyleSheet.create<Style>({
   },
 });
 
-interface Props<T> {
-  date?: Date;
-  onDateChanged?: (date: Date) => void;
-  selectedDate?: Date;
-  selectDate?: (date: Date) => void;
-}
-
 const isToday = (date: Date): boolean => {
   const today = new Date();
   return date.getDate() === today.getDate() &&
@@ -137,24 +130,20 @@ const isToday = (date: Date): boolean => {
       date.getFullYear() === today.getFullYear();
 };
 
+interface Props<T> {
+  date?: Date;
+  onDateChanged?: (date: Date) => void;
+  selectedDate?: Date;
+  selectDate?: (date: Date) => void;
+}
+
 function CalendarCarousel<T>({
   date = new Date(), onDateChanged, selectDate, selectedDate,
-}: PropsWithChildren<Props<T>>): ReactElement {
+}: PropsWithChildren<Props<T>>): React.ReactElement {
   const [currentDate, setCurrentDate] = useState<Date>(date);
-  const monthName = currentDate.toLocaleString('default', {
-    month: 'long',
-  });
-
-  const weekdays = [];
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth();
-
-  const currentMonthDays = new Date(year, month + 1, 0).getDate();
-  const firstWeekday = new Date(year, month, 1).getDay();
-  const lastWeekday = new Date(year, month, currentMonthDays).getDay();
-  let numPrevMonthDays = new Date(year, month - 1, 0).getDate();
 
   const changeMonth = (toPrevMonth?: boolean): void => {
+    console.log('changeMonth');
     if (toPrevMonth) {
       const update = new Date(
         currentDate.getFullYear(),
@@ -176,91 +165,107 @@ function CalendarCarousel<T>({
     return onDateChanged?.(update);
   };
 
-  // TODO: Provide drawing calendar as a one single function to reuse it in other pages.
-  for (let idx = 0; idx <= 6; idx++) {
-    const matchMonth = new Date(2020, 5, idx);
-    const weekDay = matchMonth.toLocaleString('default', { weekday: 'narrow' });
-    weekdays.push(
-      <View style={{ width: 47.14 }} key={idx}>
-        <Text style={styles.weekdayText}>{weekDay}</Text>
-      </View>,
-    );
-  }
+  const prevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate());
+  const currentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+  const nextMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate());
 
-  const prevDays = [];
-  for (let idx = 0; idx < firstWeekday; idx++) {
-    prevDays.unshift(
-      <View style={styles.otherDaysView}>
-        <Text style={styles.otherDaysText}>{`${numPrevMonthDays}`}</Text>
-      </View>,
-    );
-    numPrevMonthDays--;
-  }
+  const renderCalendar = (currentDate: Date): any => {
+    const monthName = currentDate.toLocaleString('default', {
+      month: 'long',
+    });
+    const weekdays = [];
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
 
-  const days = [];
-  for (let d = 1; d <= currentMonthDays; d++) {
-    if (isToday(
-      new Date(
-        currentDate.getFullYear(),
-        currentDate.getMonth(),
-        d,
-      ),
-    )) {
-      days.push(
-        <View
-          style={styles.currentDayView}>
-          <Text style={styles.currentDayText}>{`${d}`}</Text>
+    const currentMonthDays = new Date(year, month + 1, 0).getDate();
+    const firstWeekday = new Date(year, month, 1).getDay();
+    const lastWeekday = new Date(year, month, currentMonthDays).getDay();
+    let numPrevMonthDays = new Date(year, month - 1, 0).getDate();
+
+    // TODO: Provide drawing calendar as a one single function to reuse it in other pages.
+    for (let idx = 0; idx <= 6; idx++) {
+      const matchMonth = new Date(2020, 5, idx);
+      const weekDay = matchMonth.toLocaleString('default', { weekday: 'narrow' });
+      weekdays.push(
+        <View style={{ width: 47.14 }} key={idx}>
+          <Text style={styles.weekdayText}>{weekDay}</Text>
         </View>,
       );
-    } else if (d === selectedDate?.getDate() && month === selectedDate?.getMonth()) {
-      days.push(
-        <TouchableOpacity onPress={(): void => selectDate?.(
+    }
+
+    const prevDays = [];
+    for (let idx = 0; idx < firstWeekday; idx++) {
+      prevDays.unshift(
+        <View style={styles.otherDaysView}>
+          <Text style={styles.otherDaysText}>{`${numPrevMonthDays}`}</Text>
+        </View>,
+      );
+      numPrevMonthDays--;
+    }
+
+    const days = [];
+    for (let d = 1; d <= currentMonthDays; d++) {
+      if (isToday(
+        new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth(),
+          d,
+        ),
+      )) {
+        days.push(
+          <View
+            style={styles.currentDayView}>
+            <Text style={styles.currentDayText}>{`${d}`}</Text>
+          </View>,
+        );
+      } else if (d === selectedDate?.getDate() && month === selectedDate?.getMonth()) {
+        days.push(
+          <TouchableOpacity onPress={(): void => selectDate?.(
           new Date(
             currentDate.getFullYear(),
             currentDate.getMonth(),
             d,
           ),
         )}>
-          <View style={styles.activeView}>
-            <Text style={styles.activeText}>{`${d}`}</Text>
-          </View>
-        </TouchableOpacity>,
-      );
-    } else {
-      days.push(
-        <TouchableOpacity onPress={(): void => selectDate?.(
+            <View style={styles.activeView}>
+              <Text style={styles.activeText}>{`${d}`}</Text>
+            </View>
+          </TouchableOpacity>,
+        );
+      } else {
+        days.push(
+          <TouchableOpacity onPress={(): void => selectDate?.(
           new Date(
             currentDate.getFullYear(),
             currentDate.getMonth(),
             d,
           ),
         )}>
-          <View style={styles.notActiveView}>
-            <Text style={styles.notActiveText}>{`${d}`}</Text>
-          </View>
-        </TouchableOpacity>,
-      );
+            <View style={styles.notActiveView}>
+              <Text style={styles.notActiveText}>{`${d}`}</Text>
+            </View>
+          </TouchableOpacity>,
+        );
+      }
     }
-  }
 
-  const nextDays = [];
-  if (6 - lastWeekday >= 1) {
-    for (let idx = 1; idx <= 6 - lastWeekday; idx++) {
-      nextDays.push(
-        <View
-          style={styles.otherDaysView}>
-          <Text
-            style={styles.otherDaysText}>
-            {`${idx}`}
-          </Text>
-        </View>,
-      );
+    const nextDays = [];
+    if (6 - lastWeekday >= 1) {
+      for (let idx = 1; idx <= 6 - lastWeekday; idx++) {
+        nextDays.push(
+          <View
+            style={styles.otherDaysView}>
+            <Text
+              style={styles.otherDaysText}>
+              {`${idx}`}
+            </Text>
+          </View>,
+        );
+      }
     }
-  }
 
-  const calendarDays = [...prevDays, ...days, ...nextDays];
+    const calendarDays = [...prevDays, ...days, ...nextDays];
 
-  const renderHeader = () : React.ReactElement => {
     return (
       <Fragment>
         <View style={styles.headerStyle}>
@@ -276,38 +281,40 @@ function CalendarCarousel<T>({
           </TouchableOpacity>
         </View>
         <View style={styles.weekdayStyle}>{weekdays}</View>
+        <FlatList
+          style={styles.dayContainer}
+          numColumns={7}
+          data={calendarDays}
+          renderItem={({ item }): ReactElement => {
+            return item;
+          }}
+          keyExtractor={(item, id): string => id.toString()}
+        />
       </Fragment>
+
     );
-  };
-
-  const renderMonth = (type?: MonthType): ReactElement => {
-    // TODO: differentiate months to render with `MonthType
-
-    return <FlatList
-      style={styles.dayContainer}
-      numColumns={7}
-      data={calendarDays}
-      renderItem={({ item }): ReactElement => {
-        return item;
-      }}
-      keyExtractor={(item, id): string => id.toString()}
-    />;
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      { renderHeader() }
       <ScrollView
         horizontal
         pagingEnabled
+        onMomentumScrollBegin={(e): void => {
+          console.log('momentScroll', e);
+        }}
         onScrollEndDrag={(e): void => {
           // TODO: You can see targetContentOffset to check current page
           console.log('onScrollEndDrag', e);
         }}
+        onScrollBeginDrag= {(e): void => {
+          // TODO: You can see targetContentOffset to check current page
+
+        }}
       >
-        {renderMonth()}
-        {renderMonth()}
-        {renderMonth()}
+        {renderCalendar(prevMonth)}
+        {renderCalendar(currentMonth)}
+        {renderCalendar(nextMonth)}
       </ScrollView>
     </SafeAreaView>
   );
