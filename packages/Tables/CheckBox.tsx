49a91f8a7ecd413f8584ffd5cb03f71419461d84
luyoes <60371244+LUYOES@users.noreply.github.com>
2020-08-02 13:19:02 +0900

    fix: remove useless values imported [CheckBox.test, Snackbar.test] (#280)

--- packages/Tables/CheckBox.tsx
+++ packages/Tables/CheckBox.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import styled from 'styled-components/native';
 
