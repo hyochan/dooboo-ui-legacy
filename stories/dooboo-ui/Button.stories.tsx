16aa95b677179ba617b3b654f0092be744124f6e
sk97kang <34053363+sk97kang@users.noreply.github.com>
2020-08-16 21:22:39 +0900

    Add ScrollContainer in stories/Button

--- stories/dooboo-ui/Button.stories.tsx
+++ stories/dooboo-ui/Button.stories.tsx
@@ -9,12 +9,17 @@ import { storiesOf } from '@storybook/react-native';
 import styled from 'styled-components/native';
 import { text } from '@storybook/addon-knobs';
 
+const ScrollContainer = styled.ScrollView`
+  width: 100%;
+`;
+
 const Container = styled.View`
   background-color: transparent;
   align-items: center;
   justify-content: center;
   width: 100%;
   margin-top: 20px;
+  margin-bottom: 20px;
   flex-direction: column;
 `;
 
@@ -23,127 +28,129 @@ function Default(): React.ReactElement {
   const [googleLoading, setGoogleLoading] = useState<boolean>(false);
 
   return (
-    <Container>
-      <Button
-        isLoading={false}
-        text="😀 😎 👍 💯"
-        onPress={action('Clicked')}
-        style={{
-          marginVertical: 40,
-        }}
-      />
-      <Button
-        style={{
-          backgroundColor: '#109CF1',
-        }}
-        hoverStyle={{
-          backgroundColor: '#34AFF9',
-        }}
-        accentStyle={{
-          shadowColor: 'black',
-          shadowOffset: {
-            width: 0,
-            height: 4,
-          },
-          shadowOpacity: 0.24,
-          shadowRadius: 16.0,
-          elevation: 10,
-          borderRadius: 4,
-        }}
-        textStyle={{ color: '#FFFFFF' }}
-        onPress={action('Clicked')}
-        text={'Accent button '}
-      />
+    <ScrollContainer>
+      <Container>
+        <Button
+          isLoading={false}
+          text="😀 😎 👍 💯"
+          onPress={action('Clicked')}
+          style={{
+            marginVertical: 40,
+          }}
+        />
+        <Button
+          style={{
+            backgroundColor: '#109CF1',
+          }}
+          hoverStyle={{
+            backgroundColor: '#34AFF9',
+          }}
+          accentStyle={{
+            shadowColor: 'black',
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.24,
+            shadowRadius: 16.0,
+            elevation: 10,
+            borderRadius: 4,
+          }}
+          textStyle={{ color: '#FFFFFF' }}
+          onPress={action('Clicked')}
+          text={'Accent button '}
+        />
 
-      <Button
-        hoverTextStyle={{
-          color: '#34AFF9',
-        }}
-        secondaryStyle={{
-          borderColor: '#109CF1',
-          borderWidth: 2,
-          shadowColor: 'black',
-          shadowOffset: {
-            width: 0,
-            height: 4,
-          },
-          shadowOpacity: 0.24,
-          shadowRadius: 16.0,
-          elevation: 10,
-          borderRadius: 4,
-        }}
-        style={{
-          marginVertical: 40,
-        }}
-        textStyle={{ color: '#109CF1' }}
-        onPress={action('Clicked')}
-        text={'Secondary'}
-      />
-      <Button
-        style={{
-          marginVertical: 40,
-        }}
-        isDisabled={true}
-        text={text('button text', 'this is disabled')}
-      />
-      <Button
-        leftElement={
-          <View
-            style={{
-              position: 'absolute',
-              left: 16,
-            }}>
-            <Image style={{ width: 20, height: 20 }} source={IC_GOOGLE} />
-          </View>
-        }
-        isLoading={googleLoading}
-        indicatorColor="#023059"
-        containerStyle={{
-          marginTop: 32,
-        }}
-        style={{
-          backgroundColor: '#ccc',
-        }}
-        onPress={(): void => {
-          setGoogleLoading(true);
-          const timeout = setTimeout(() => {
-            setGoogleLoading(false);
-            clearTimeout(timeout);
-          }, 2000);
-        }}
-        text="GOOGLE SIGN IN"
-      />
-      <Button
-        testID="btnFacebook"
-        leftElement={
-          <View
-            style={{
-              position: 'absolute',
-              left: 16,
-            }}>
-            <Image style={{ width: 15, height: 28 }} source={IC_FACEBOOK} />
-          </View>
-        }
-        indicatorColor="#023059"
-        isLoading={facebookLoading}
-        containerStyle={{
-          marginTop: 32,
-        }}
-        style={{
-          backgroundColor: '#ccc',
-          borderWidth: 0.5,
-          borderRadius: 0,
-        }}
-        onPress={(): void => {
-          setFacebookLoading(true);
-          const timeout = setTimeout(() => {
-            setFacebookLoading(false);
-            clearTimeout(timeout);
-          }, 2000);
-        }}
-        text="FACEBOOK SIGN IN"
-      />
-    </Container>
+        <Button
+          hoverTextStyle={{
+            color: '#34AFF9',
+          }}
+          secondaryStyle={{
+            borderColor: '#109CF1',
+            borderWidth: 2,
+            shadowColor: 'black',
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.24,
+            shadowRadius: 16.0,
+            elevation: 10,
+            borderRadius: 4,
+          }}
+          style={{
+            marginVertical: 40,
+          }}
+          textStyle={{ color: '#109CF1' }}
+          onPress={action('Clicked')}
+          text={'Secondary'}
+        />
+        <Button
+          style={{
+            marginVertical: 40,
+          }}
+          isDisabled={true}
+          text={text('button text', 'this is disabled')}
+        />
+        <Button
+          leftElement={
+            <View
+              style={{
+                position: 'absolute',
+                left: 16,
+              }}>
+              <Image style={{ width: 20, height: 20 }} source={IC_GOOGLE} />
+            </View>
+          }
+          isLoading={googleLoading}
+          indicatorColor="#023059"
+          containerStyle={{
+            marginTop: 32,
+          }}
+          style={{
+            backgroundColor: '#ccc',
+          }}
+          onPress={(): void => {
+            setGoogleLoading(true);
+            const timeout = setTimeout(() => {
+              setGoogleLoading(false);
+              clearTimeout(timeout);
+            }, 2000);
+          }}
+          text="GOOGLE SIGN IN"
+        />
+        <Button
+          testID="btnFacebook"
+          leftElement={
+            <View
+              style={{
+                position: 'absolute',
+                left: 16,
+              }}>
+              <Image style={{ width: 15, height: 28 }} source={IC_FACEBOOK} />
+            </View>
+          }
+          indicatorColor="#023059"
+          isLoading={facebookLoading}
+          containerStyle={{
+            marginTop: 32,
+          }}
+          style={{
+            backgroundColor: '#ccc',
+            borderWidth: 0.5,
+            borderRadius: 0,
+          }}
+          onPress={(): void => {
+            setFacebookLoading(true);
+            const timeout = setTimeout(() => {
+              setFacebookLoading(false);
+              clearTimeout(timeout);
+            }, 2000);
+          }}
+          text="FACEBOOK SIGN IN"
+        />
+      </Container>
+    </ScrollContainer>
   );
 }
 
