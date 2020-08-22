2f4414115e51fc1d30abfb124f23d4b811f42e4a
luyoes <luyoes20@gmail.com>
2020-08-22 17:49:53 +0900

    title recommit

--- main/Card/index.tsx
+++ main/Card/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode, ReactNodeArray } from 'react';
-import { ViewStyle, ImageSourcePropType, ImageStyle } from 'react-native';
+import { ImageSourcePropType, ImageStyle, TextStyle, ViewProps, ViewStyle } from 'react-native';
 
 import styled from 'styled-components/native';
 
@@ -21,6 +21,19 @@ const StlyedImage = styled.Image`
   height: 100px;
 `;
 
+const TitleContainer = styled.View<TitleContainerProps>`
+  justify-content: ${(props) => (props.titleTextVertical ? 'flex-start' : 'center')};
+  padding: 5px 10px;
+`;
+
+const TitleText = styled.Text`
+  font-size: 13px;
+`;
+
+const SubTitleText = styled.Text`
+  font-size: 10px;
+`;
+
 interface Props {
   testID?: string;
   containerStyle?: ViewStyle;
@@ -28,13 +41,43 @@ interface Props {
   image?: ImageSourcePropType;
   imageStyle?: ImageStyle;
   contentsStyle?: ViewStyle;
+
+  titleContainerStyle?: ViewStyle;
+  title?: string;
+  titleStyle?: TextStyle;
+  subTitle?: string;
+  subTitleStyle?: TextStyle;
+}
+
+interface TitleContainerProps extends ViewProps {
+  titleTextVertical?: boolean;
 }
 
 const Card: FC<Props> = (props) => {
-  const { containerStyle, children, image, imageStyle, contentsStyle } = props;
+  const {
+    containerStyle,
+    children,
+    image,
+    imageStyle,
+    contentsStyle,
+    titleContainerStyle,
+    title,
+    titleStyle,
+    subTitle,
+    subTitleStyle,
+  } = props;
+  const titleVertical = !!(subTitle !== undefined && subTitle.length > 0);
   return (
     <Container style={[containerStyle]}>
       {image && <StlyedImage source={image} style={[imageStyle]} />}
+
+      <TitleContainer style={[titleContainerStyle]} titleTextVertical={ titleVertical } >
+        <TitleText style={[titleStyle]} > {title} </TitleText>
+        { subTitle && subTitle.length > 0
+          ? <SubTitleText style={[subTitleStyle]}> {subTitle} </SubTitleText>
+          : null }
+      </TitleContainer>
+
       {children && (
         <ContentsContainer style={[contentsStyle]}>
           {children}
@@ -44,6 +87,27 @@ const Card: FC<Props> = (props) => {
   );
 };
 
-Card.defaultProps = {};
+Card.defaultProps = {
+  titleContainerStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    width: '100%',
+    height: 'auto',
+    backgroundColor: 'transparent',
+  },
+  titleStyle: {
+    backgroundColor: 'transparent',
+    color: '#000000',
+
+  },
+  subTitleStyle: {
+    backgroundColor: 'transparent',
+    color: '#e4e4e4',
+    height: 20,
+  },
+
+};
 
 export { Card };
