a3d1f271192e35aa88779af636a3defce78abc6f
Terry <terry@dooboolab.com>
2020-08-30 00:26:03 +0900

    Add ImageSlider to PinchZoom examples

--- stories/dooboo-ui/PinchZoom.stories.tsx
+++ stories/dooboo-ui/PinchZoom.stories.tsx
@@ -1,10 +1,10 @@
-import { Article, ImageList } from '../../main/PinchZoom/PinchZoom.example';
+import { Article, ImageList, ImageSlider } from '../../main/PinchZoom/PinchZoom.example';
 import { ContainerDeco } from '../../storybook/decorators';
-import { PinchZoom } from '../../main';
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 
 storiesOf('PinchZoom', module)
   .addDecorator(ContainerDeco)
-  .add('Image list', () => <ImageList PinchZoom={PinchZoom} />)
-  .add('Text block', () => <Article PinchZoom={PinchZoom} />);
+  .add('Image list', () => <ImageList />)
+  .add('Text block', () => <Article />)
+  .add('Image Slider', () => <ImageSlider />);
