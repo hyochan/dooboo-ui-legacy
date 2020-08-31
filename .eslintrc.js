bad08700845d912552fcf2470dadd65643506eef
Terry <terry@dooboolab.com>
2020-08-31 13:55:58 +0900

    Use @typescript-eslint/no-unused-expressions to support optional chaining

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,14 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/ban-ts-comment': 0,
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
   },
 };
