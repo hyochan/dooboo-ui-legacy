1842cbf4f7f13274b90d350084962c4293d3bf3a
greatSumini <greatsumini@gmail.com>
2020-09-12 01:40:56 +0900

    fix: :bug: fix shadowOffset style code
    - nested object ro coordiname ({width: 0, height: 2px} -> 0 2px;)
    - to remove Node of type rule not supported as an inline style warning

--- main/Card/styles.tsx
+++ main/Card/styles.tsx
@@ -1,18 +1,18 @@
 import { css } from 'styled-components/native';
 
 const shadow = css`
-    shadow-color: #000;
-    shadow-offset: { width: 0, height: 2 };
-    shadow-opacity: 0.25;
-    shadow-radius: 4px;
-    elevation: 5;
+  shadow-color: #000;
+  shadow-offset: 0 2px;
+  shadow-opacity: 0.25;
+  shadow-radius: 4px;
+  elevation: 5;
 `;
 
 const raisedShadow = css`
   shadow-color: #000;
-  shadow-offset: { width: 0, height: 5 };
-  shadow-opacity: 0.34;
-  shadow-radius: 6.27px;
+  shadow-offset: 0 6px;
+  shadow-opacity: 0.35;
+  shadow-radius: 6.25px;
   elevation: 10;
 `;
 
