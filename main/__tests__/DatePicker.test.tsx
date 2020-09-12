3647e3a5dec6073bdadbdd066c6b821819917b55
oh3vci <oh3vci@gmail.com>
2020-09-12 20:32:55 +0900

    test: add onPress event and cases of CalendarDate and CalendarWeekDays

--- main/__tests__/DatePicker.test.tsx
+++ main/__tests__/DatePicker.test.tsx
@@ -1,40 +1,100 @@
 import * as React from 'react';
-
+import CalendarDate from '../DatePicker/Calendar/CalendarDate';
+import CalendarWeekDays from '../DatePicker/Calendar/CalendarWeekDays';
 import DateInput from '../DatePicker/DateInput';
 import { DatePicker } from '../DatePicker';
 import PickerCalendar from '../DatePicker/PickerCalendar';
-
+import { TouchableOpacity } from 'react-native';
 import { render } from '@testing-library/react-native';
+import renderer from 'react-test-renderer';
 
-describe('[DateInput] render', () => {
+describe('[DatePicker] render', () => {
   it('should render without crashing', () => {
-    const rendered = render(
-      <DateInput
-        onPressCalendar={(): void => {}}
-      />,
-    ).asJSON();
+    const rendered = render(<DatePicker />).asJSON();
     expect(rendered).toMatchSnapshot();
     expect(rendered).toBeTruthy();
   });
 });
 
-describe('[DatePicker] render', () => {
+describe('[DateInput] render', () => {
   it('should render without crashing', () => {
     const rendered = render(
-      <DatePicker />,
+      <DateInput onPressCalendar={(): void => {}} />,
     ).asJSON();
     expect(rendered).toMatchSnapshot();
     expect(rendered).toBeTruthy();
+  });
+  describe('[DateInput] Interaction', () => {
+    it('should simulate onPress', () => {
+      const onPressCalendar = jest.fn();
+      const rendered = renderer.create(
+        <DateInput onPressCalendar={onPressCalendar} />,
+        {
+          createNodeMock: () => {
+            return {
+              onPressCalendarIn: onPressCalendar,
+              onPressCalendarOut: onPressCalendar,
+            };
+          },
+        },
+      );
+
+      const dateInputClicked = rendered.root.findByType(TouchableOpacity);
+      renderer.act(() => {
+        dateInputClicked.props.onPress();
+      });
+      expect(onPressCalendar).toHaveBeenCalled();
+    });
   });
 });
 
 describe('[PickerCalendar] render', () => {
   it('should render without crashing', () => {
     const rendered = render(
-      <PickerCalendar
-        visible={false}
-        onSelectDate={(): void => {}}
-      />,
+      <PickerCalendar visible={false} onSelectDate={(): void => {}} />,
+    ).asJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+});
+
+describe('[CalendarDate] render', () => {
+  it('should render without crashing', () => {
+    const rendered = render(
+      <CalendarDate onPress={(): void => {}} date={new Date()} />,
+    ).asJSON();
+    expect(rendered).toMatchSnapshot();
+    expect(rendered).toBeTruthy();
+  });
+
+  describe('[CalendarDate] Interaction', () => {
+    it('should simulate onPress', () => {
+      const onSelectDate = jest.fn();
+      const rendered = renderer.create(
+        <CalendarDate onPress={onSelectDate} date={new Date()} />,
+        {
+          createNodeMock: () => {
+            return {
+              onSelectDateIn: onSelectDate,
+              onSelectDateOut: onSelectDate,
+            };
+          },
+        },
+      );
+
+      const dateSelected = rendered.root.findByType(TouchableOpacity);
+      renderer.act(() => {
+        dateSelected.props.onPress();
+      });
+      expect(onSelectDate).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('[CalendarWeekDays] render', () => {
+  it('should render without crashing', () => {
+    const rendered = render(
+      <CalendarWeekDays calendarWidth={300} weekdayFormat={'narrow'} />,
     ).asJSON();
     expect(rendered).toMatchSnapshot();
     expect(rendered).toBeTruthy();
