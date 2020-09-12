18f82cfdcf94bea36c581a85f932f447380a3d34
dahee kim <ekekekgml@khu.ac.kr>
2020-09-12 15:11:58 +0900

    test: :white_check_mark: add initial test code

--- main/__tests__/Checkbox.test.tsx
+++ main/__tests__/Checkbox.test.tsx
@@ -1,18 +1,60 @@
 import * as React from 'react';
 import { Checkbox } from '../Checkbox';
+import { TouchableHighlight } from 'react-native';
+import renderer from 'react-test-renderer';
 
-import {
-  render,
-} from '@testing-library/react-native';
+let props: unknown;
 
 // Note: test renderer must be required after react-native.
+const createTestProps = (obj: Record<string, unknown>): Record<string, unknown> => ({
+  navigation: {
+    navigate: jest.fn(),
+  },
+  ...obj,
+});
+
+const component = (props?): React.ReactElement => {
+  return <Checkbox {...props}/>;
+};
 
 describe('[CheckBox]', () => {
-  const defaultPros = {
-    label: 'testProps',
-  };
+  beforeEach(() => {
+    props = createTestProps({});
+  });
+
   it('render without crashing', () => {
-    const rendered = render(<Checkbox {...defaultPros}/>);
+    const rendered: renderer.ReactTestRendererJSON | null = renderer
+      .create(component(props))
+      .toJSON();
     expect(rendered).toMatchSnapshot();
+  });
+
+  describe('[CheckBox] Interaction', (): void => {
+    it('should simulate onPress', (): void => {
+      const handlePress = jest.fn();
+      const rendered = renderer.create(
+        component({
+          label: 'CHECKBOX_LABEL',
+          onChange: handlePress,
+        }),
+      );
+
+      const checkboxClick = rendered.root.findByType(TouchableHighlight);
+      renderer.act(() => {
+        checkboxClick.props.onPress();
+      });
+      expect(handlePress).toHaveBeenCalled();
+    });
+    it('should simulate props', (): void => {
+      const rendered = renderer.create(
+        component({
+          label: 'CHECKBOX_LABEL',
+        }),
+      );
+
+      rendered.update(component({ disabled: true }));
+      expect(rendered).toMatchSnapshot();
+      expect(rendered).toBeTruthy();
+    });
   });
 });
