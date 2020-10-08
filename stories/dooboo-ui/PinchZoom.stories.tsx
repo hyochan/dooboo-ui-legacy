e53fdcc61facbc6cbed14f095ddd8e3dc5513849
Jongtaek Choi <lastchoice86@naver.com>
2020-10-08 18:11:44 +0900

    Reset to initial setting of pinchzoom

--- stories/dooboo-ui/PinchZoom.stories.tsx
+++ stories/dooboo-ui/PinchZoom.stories.tsx
@@ -1,11 +1,15 @@
-import { Article, ImageList, ImageSlider } from '../../packages/PinchZoom/PinchZoom.example';
-
 import { ContainerDeco } from '../../storybook/decorators';
+import { Dimensions, Image } from 'react-native';
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
+import PinchZoom from '../../packages/PinchZoom';
+
+const imageSource = 'https://user-images.githubusercontent.com/17980230/95435131-29210280-098d-11eb-92e6-8b6dd49032b3.jpeg'
 
 storiesOf('PinchZoom', module)
   .addDecorator(ContainerDeco)
-  .add('Image list', () => <ImageList />)
-  .add('Text block', () => <Article />)
-  .add('Image Slider', () => <ImageSlider />);
+  .add('Image', () => <PinchZoom style={{ width: '100%' }}>
+    <Image
+      source={{ uri: imageSource }}
+      style={{ width: '100%', height: Dimensions.get('screen').width * 4 / 3 }} />
+  </PinchZoom>)
