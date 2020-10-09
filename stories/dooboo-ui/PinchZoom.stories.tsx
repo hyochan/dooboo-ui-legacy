2a480021cc079cb07178a320d08356cac1267ff2
Jongtaek Choi <lastchoice86@naver.com>
2020-10-09 22:51:22 +0900

    Add changing scale function

--- stories/dooboo-ui/PinchZoom.stories.tsx
+++ stories/dooboo-ui/PinchZoom.stories.tsx
@@ -1,10 +1,11 @@
-import { ContainerDeco } from '../../storybook/decorators';
 import { Dimensions, Image } from 'react-native';
+
+import { ContainerDeco } from '../../storybook/decorators';
+import PinchZoom from '../../packages/PinchZoom';
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
-import PinchZoom from '../../packages/PinchZoom';
 
-const imageSource = 'https://user-images.githubusercontent.com/17980230/95435131-29210280-098d-11eb-92e6-8b6dd49032b3.jpeg'
+const imageSource = 'https://user-images.githubusercontent.com/17980230/95435131-29210280-098d-11eb-92e6-8b6dd49032b3.jpeg';
 
 storiesOf('PinchZoom', module)
   .addDecorator(ContainerDeco)
@@ -12,4 +13,4 @@ storiesOf('PinchZoom', module)
     <Image
       source={{ uri: imageSource }}
       style={{ width: '100%', height: Dimensions.get('screen').width * 4 / 3 }} />
-  </PinchZoom>)
+  </PinchZoom>);
