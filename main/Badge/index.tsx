459c0d3e8107f618018aee55e0cf86fd6973a34e
Hyo <dooboolab@gmail.com>
2020-10-15 01:31:31 +0900

    Update index.tsx

--- main/Badge/index.tsx
+++ main/Badge/index.tsx
@@ -17,7 +17,7 @@ export interface BadgeProps extends StyleProps {
   maximumCount?: number;
   showZero?: boolean;
   opacityVisible?: boolean;
-  variant?: 'standard' | 'dot;
+  variant?: 'standard' | 'dot';
 }
 
 const StyledView = styled.View<StyleProps>`
