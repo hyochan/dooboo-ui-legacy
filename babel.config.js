e66b4646072234a3311bb8d08fa0af5ae8308ced
hyochan <dooboolab@gmail.com>
2021-01-01 18:32:48 +0900

    Added preset react to project

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,15 @@
 module.exports = function(api) {
   api.cache(true);
+
   return {
     presets: [
+      '@babel/preset-react',
       'babel-preset-expo',
       '@babel/preset-typescript',
     ],
     plugins: [
-      "@babel/plugin-syntax-dynamic-import",
-      "@babel/plugin-transform-modules-commonjs",
-    ]
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-transform-modules-commonjs',
+    ],
   };
 };
