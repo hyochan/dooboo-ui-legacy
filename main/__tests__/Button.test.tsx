6c5869fbaecd6abba8ffcc6319044b52d6ea828f
hyochan <dooboolab@gmail.com>
2021-02-04 00:31:52 +0900

    Migrated [Button] tests to react-native-testing-library with 100% coverage

--- main/__tests__/Button.test.tsx
+++ main/__tests__/Button.test.tsx
@@ -1,66 +1,204 @@
 import * as React from 'react';
 
-import {Button} from '../../main';
-import {Text} from 'react-native';
-import renderer from 'react-test-renderer';
+import {act, fireEvent, render, waitFor} from '@testing-library/react-native';
 
-const component = (props?: any): React.ReactElement => {
+import {Button} from '../../main';
+import type {ButtonProps} from '../../main/Button';
+import {LoadingIndicator} from '../LoadingIndicator';
+import type {RenderAPI} from '@testing-library/react-native';
+import {Text} from 'react-native';
+
+let testingLib: RenderAPI;
+
+jest.mock('react-native-web-hooks', () => ({
+  useHover: () => true,
+}));
+
+const component = (props?: ButtonProps): React.ReactElement => {
   return <Button {...props} />;
 };
 
 describe('[Button]', () => {
-  let rendered: renderer.ReactTestRenderer;
-  let root: renderer.ReactTestInstance;
-
   it('should render without crashing', () => {
-    rendered = renderer.create(component());
-    // expect(rendered.toJSON()).toMatchSnapshot();
-    expect(rendered.toJSON()).toBeTruthy();
+    testingLib = render(component());
+
+    const json = testingLib.toJSON();
+
+    expect(json).toBeTruthy();
   });
 
-  it('should render loading status', () => {
-    rendered = renderer.create(component({loading: true}));
-    root = rendered.root;
+  describe('Loading', () => {
+    it('should render loading status', () => {
+      testingLib = render(component({loading: true}));
 
-    const texts = root.findAllByType(Text);
+      expect(LoadingIndicator).toBeDefined();
+      expect(testingLib.getByTestId('loading-view')).toBeTruthy();
+    });
 
-    expect(texts).toHaveLength(0);
+    it('should render default disabled style when disabled', () => {
+      testingLib = render(
+        component({
+          loading: true,
+          disabled: true,
+        }),
+      );
+
+      expect(
+        testingLib.getByTestId('loading-view').props.style[4],
+      ).toBeTruthy();
+    });
+
+    it('should render disabled button style when disabled', () => {
+      testingLib = render(
+        component({
+          loading: true,
+          disabled: true,
+          styles: {
+            disabledButton: {
+              borderColor: 'red',
+            },
+          },
+        }),
+      );
+
+      const disabledButtonStyle = testingLib.getByTestId('loading-view').props
+        .style[4][1];
+
+      expect(disabledButtonStyle).toEqual({
+        borderColor: 'red',
+      });
+    });
+
+    it('should render custom container', () => {
+      testingLib = render(
+        component({
+          loading: true,
+          disabled: false,
+          styles: {
+            container: {
+              borderRadius: 0,
+            },
+          },
+        }),
+      );
+
+      expect(testingLib.getByTestId('loading-view')).toBeTruthy();
+    });
   });
 
-  it('should render disabled status', () => {
-    rendered = renderer.create(component({disabled: true}));
-    root = rendered.root;
+  describe('Button', () => {
+    it('should render disabled status', () => {
+      testingLib = render(
+        component({
+          disabled: true,
+        }),
+      );
 
-    const texts = root.findAllByType(Text);
+      const json = testingLib.toJSON();
 
-    expect(texts).toHaveLength(1);
+      expect(json).toBeTruthy();
+    });
+
+    it('should render disabled status with disabled style', () => {
+      testingLib = render(
+        component({
+          disabled: true,
+          styles: {
+            disabledButton: {
+              backgroundColor: 'yellow',
+            },
+          },
+        }),
+      );
+
+      const button = testingLib.getByTestId('button-view');
+      const disabledButtonStyle = button.props.style[3];
+
+      const found = disabledButtonStyle.some(
+        (el) => el.backgroundColor === 'yellow',
+      );
+
+      expect(found).toBeTruthy();
+    });
+
+    it('should render container', () => {
+      testingLib = render(
+        component({
+          styles: {
+            container: {
+              backgroundColor: 'blue',
+            },
+          },
+        }),
+      );
+
+      const button = testingLib.getByTestId('button-view');
+      const buttonContainerStyle = button.props.style[1];
+
+      expect(buttonContainerStyle).toEqual({
+        backgroundColor: 'blue',
+      });
+    });
+  });
+
+  describe('After onLayout', () => {
+    it('should trigger onLayout then set loading-view layout', async () => {
+      testingLib = render(component());
+
+      const button = testingLib.getByTestId('button-view');
+
+      act(() => {
+        button.props.onLayout({
+          nativeEvent: {
+            layout: {
+              width: 375,
+              height: 667,
+            },
+          },
+        });
+      });
+
+      testingLib.rerender(
+        component({
+          loading: true,
+        }),
+      );
+
+      const loading = await waitFor(() =>
+        testingLib.getByTestId('loading-view'),
+      );
+
+      const buttonLayoutStyle = loading.props.style[2];
+
+      expect(buttonLayoutStyle.width).toEqual(375);
+      expect(buttonLayoutStyle.height).toEqual(667);
+    });
   });
 
   it('should render left and right elements', () => {
-    rendered = renderer.create(
+    testingLib = render(
       component({leftElement: <Text />, rightElement: <Text />}),
     );
 
-    root = rendered.root;
+    const button = testingLib.getByTestId('button-view');
 
-    const texts = root.findAllByType(Text);
-
-    expect(texts).toHaveLength(3);
+    expect(button.findAllByType(Text)).toHaveLength(3);
   });
 
   describe('[Button] Interaction', () => {
     let cnt = 1;
 
-    it('simulate onPress', () => {
-      rendered = renderer.create(
+    it('should simulate onPress', () => {
+      testingLib = render(
         component({
-          onClick: () => cnt++,
+          onPress: () => cnt++,
         }),
       );
 
-      root = rendered.root;
+      const button = testingLib.getByTestId('button-view');
 
-      root.findByType(Button).props.onClick();
+      fireEvent.press(button);
+
       expect(cnt).toBe(2);
     });
   });
