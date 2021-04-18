3f1beee3eb68dbca68b03d2f0fdf0c4224f9a2e8
hyochan <dooboolab@gmail.com>
2021-04-18 11:56:44 +0900

    Enhance emotion typescript support

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,15 @@ module.exports = (api) => {
 
   return {
     presets: [
-      '@babel/preset-react',
       'babel-preset-expo',
       '@babel/preset-typescript',
+      [
+        '@babel/preset-react',
+        {runtime: 'automatic', importSource: '@emotion/react'},
+      ],
     ],
     plugins: [
+      '@emotion/babel-plugin',
       '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-transform-modules-commonjs',
     ],
